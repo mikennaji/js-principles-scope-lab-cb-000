9f1a417850ed0c16c82cd962ae20fb602e014d09
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-22 01:12:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "kevin";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer= "Michael";
+}
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+   bestCustomer = "not bob";
+}
+function overwriteBestCustomer(val){
+  bestCustomer = val;
+}
